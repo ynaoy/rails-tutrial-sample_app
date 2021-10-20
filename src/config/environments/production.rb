d33907e8cc045e3bb45a31d78f7a3a94cf48f258
ynaoy <naozou22113@yahoo.co.jp>
2021-10-20 23:47:05 +0900

    第8章1まで、Sessionsコントローラーを追加&routを設定。login処理の見た目とそのテストだけ実装

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  #config.assets.compile = true
+  #config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -82,7 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
